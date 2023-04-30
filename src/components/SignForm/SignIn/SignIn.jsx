5ddcc367d0af688f6126611ecae508892fbd59d2
Bohdan <bvorush@gmail.com>
2023-04-30 22:28:17 +0300

    Fixed errors with components and start doing sign form

---
+++ src/components/SignForm/SignIn/SignIn.jsx
@@ -0,0 +1,35 @@
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Alert from "react-bootstrap/Alert";
+import PasswordReq from "../PasswordRequirements/PasswordReq";
+import FloatInput from "../../FloatInput/FloatInput";
+
+export default function SignIn({ email, password }) {
+  return (
+    <form>
+      <FloatInput
+        type="email"
+        label="Email"
+        onChange={email.onChange}
+        onBlur={email.onBlur}
+        value={email.value}
+      />
+      {email.isBlur && !email.inputValid && (
+        <div className="password-req">
+          <p className=" password-req__error">
+            Incorrect email format. Please, enter correct email!
+          </p>
+        </div>
+      )}
+
+      <FloatInput
+        type="password"
+        label="Password"
+        onChange={password.onChange}
+        onBlur={password.onBlur}
+        value={password.value}
+      />
+      <PasswordReq password={{ ...password }} />
+    </form>
+  );
+}
