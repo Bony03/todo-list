c339b9d9cde0219b7dbcf6efb92d92b1fb2d6fe0
Bohdan <bvorush@gmail.com>
2023-05-24 17:47:57 +0300

    added notification in profile info

--- src/store/user/loadPhotoThunk/loadPhotoThunk.js
+++ src/store/user/loadPhotoThunk/loadPhotoThunk.js
@@ -1,18 +1,36 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { setPhoto } from "../user.slice";
+import { setPhoto, logOut } from "../user.slice";
+import {
+  clearAllNotifications,
+  setProfileError,
+} from "../../system/system.slice";
 export const loadPhoto = createAsyncThunk(
   "profile/loadPhoto",
   async function (_, { dispatch }) {
-    const response = await fetch("http://192.168.31.249:3001/profile/load", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    });
-    const buffer = await response.arrayBuffer();
-    const bytes = new Uint8Array(buffer);
-    const file = new Blob([bytes.buffer]);
-    dispatch(setPhoto(file));
-    return;
+    try {
+      dispatch(clearAllNotifications());
+      const response = await fetch("http://192.168.31.249:3001/profile/load", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (!response.ok) {
+        const data = await response.json();
+        if (data.message === "Token is not valid") {
+          dispatch(logOut());
+          return;
+        }
+        throw new Error(data.message);
+      }
+      const buffer = await response.arrayBuffer();
+      const bytes = new Uint8Array(buffer);
+      const file = new Blob([bytes.buffer]);
+      dispatch(setPhoto(file));
+      return;
+    } catch (error) {
+      console.log(error.message);
+      dispatch(setProfileError(error.message));
+    }
   }
 );
